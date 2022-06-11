37b8f547086628d8899cd96bb8e935918995782c
tpietka <tomaszpietka92@gmail.com>
2022-06-11 15:52:57 +0200

    Add and configure jest

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  moduleFileExtensions: ['js', 'ts', 'json', 'vue'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.jsx?$': 'babel-jest',
+    '^.+\\.vue$': '@vue/vue3-jest',
+  },
+  testEnvironment: 'jsdom',
+};
