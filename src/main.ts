72712b616edbe3da707fba4cf897659cd929a89a
tpietka <tomaszpietka92@gmail.com>
2022-06-12 20:35:43 +0200

    Add pinia

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
+import { createPinia } from 'pinia';
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
 
 const routes = [
-    { path: '/', name: 'Home', component: () => import('./views/Home.vue') }
+    {
+        path: '/', name: 'Layout', component: () => import('./layouts/MainLayout.vue'),
+        children: [
+            { path: '/', name: 'Home', component: () => import('./views/Home.vue') },
+            { path: '/add-todo', name: 'Form', component: () => import('./views/Form.vue') }
+        ]
+    },
 ] as RouteRecordRaw[];
 
 const router = createRouter({
@@ -12,4 +18,4 @@ const router = createRouter({
     routes: routes
 })
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(createPinia()).mount('#app')
