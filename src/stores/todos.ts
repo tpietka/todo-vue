79c5121538e3419fc9890b6e39d47f876100114f
tpietka <tomaszpietka92@gmail.com>
2022-06-12 22:30:45 +0200

    Add pinia store for todos and localStorage

---
+++ src/stores/todos.ts
@@ -0,0 +1,50 @@
+import { defineStore } from 'pinia'
+import { useLocalStorage } from '../composables/useLocalStorage';
+import { Todo } from '../composables/useTodos'
+import { getCurrentDateTime } from '../helpers/date';
+
+export const useTodos = defineStore('todos', {
+    state: () => {
+        return {
+            todos: [] as Todo[],
+        }
+    },
+    actions: {
+        addTodo(todo: Todo) {
+            todo.id = this.todos.length + 1;
+            todo.created = getCurrentDateTime();
+            this.todos.push(todo);
+            localStorage.setItem('todos', JSON.stringify(this.todos));
+        },
+        markDone(id: number) {
+            const todo = this.todos.find(x => x.id == id);
+            if (todo) {
+                todo.done = true;
+                todo.completed = getCurrentDateTime();
+            }
+            localStorage.setItem('todos', JSON.stringify(this.todos));
+
+        },
+        markUndone(id: number) {
+            const todo = this.todos.find(x => x.id == id);
+            if (todo) {
+                todo.done = false;
+                todo.completed = '';
+            }
+            localStorage.setItem('todos', JSON.stringify(this.todos));
+        },
+        deleteTodo(id: number) {
+            this.todos = this.todos.filter(item => {
+                return item.id != id;
+            })
+            localStorage.setItem('todos', JSON.stringify(this.todos));
+
+        },
+        getTodos() {
+            const localStorageTodos = localStorage.getItem('todos');
+            if (localStorageTodos) {
+                this.todos = JSON.parse(localStorageTodos);
+            }
+        }
+    },
+})
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,12 @@
-<script setup lang="ts"></script>
-<template><h1>HOME</h1></template>
+<script setup lang="ts">
+import { onBeforeMount, toRefs } from 'vue';
+import TodoList from '../components/TodoList.vue';
+import { useTodos } from '../stores/todos';
+const { todos } = toRefs(useTodos());
+const { getTodos } = useTodos();
+onBeforeMount(() => {
+  getTodos();
+});
+</script>
+<template><todo-list :todos="todos"></todo-list></template>
 <style scoped></style>
