52f43dd6231db5af645f9950f5af95b4eab1f1ae
Piętka Tomasz <t.pietka@pkpcargo.com>
2022-07-03 22:51:44 +0200

    Add date validation

--- src/helpers/date.ts
+++ src/helpers/date.ts
@@ -9,3 +9,8 @@ export const getDateWithoutTime = (date: Date | string) => {
 export const getCurrentDateWithoutTime = () => {
     return new Date().toISOString().split('T')[0];
 }
+
+const dateRegex = /^\d{4}\-(0?[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])$/;
+export const isDateValid = (date: string) => {
+    return dateRegex.test(date) && Date.parse(date) != NaN;
+};
