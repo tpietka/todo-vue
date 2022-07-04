3426cfeb189b7cede46e1f505d0dae7fa61d4da1
Piętka Tomasz <t.pietka@pkpcargo.com>
2022-07-04 11:25:41 +0200

    Change todo buttons for md media query

--- src/components/TodoButtons.vue
+++ src/components/TodoButtons.vue
@@ -10,18 +10,16 @@ const { markDone, deleteTodo } = useTodos();
 </script>
 
 <template>
-  <div class="flex-column justify-between items-center px-2">
-    <div class="flex h-10 w-full gap-4">
-      <button
-        v-if="!done"
-        class="bg-green-500 text-white h-8 w-full"
-        @click="markDone(id)"
-      >
-        Done
-      </button>
-      <button class="bg-red-500 text-white h-8 w-full" @click="deleteTodo(id)">
-        Delete
-      </button>
-    </div>
+  <div class="flex justify-end px-2 h-10 w-full gap-4">
+    <button
+      v-if="!done"
+      class="bg-green-500 text-white h-8 w-full md:w-32"
+      @click="markDone(id)"
+    >
+      Done
+    </button>
+    <button class="bg-red-500 text-white h-8 w-full md:w-32" @click="deleteTodo(id)">
+      Delete
+    </button>
   </div>
 </template>
