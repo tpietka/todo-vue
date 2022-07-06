9c8cf47ac0ee326eb67d94289d8571f48169c968
Pietka Tomasz <tomaszpietka92@gmail.com>
2022-07-06 16:03:36 +0200

    Add form validation

--- src/views/Form.vue
+++ src/views/Form.vue
@@ -4,14 +4,21 @@ import { Todo } from '../models/todo';
 import { useTodos } from '../stores/todos';
 import InputLabel from '../components/InputLabel.vue';
 import { useRouter } from 'vue-router';
+import { isDateValid } from '../helpers/date';
 
 const router = useRouter();
 const { addTodo } = useTodos();
 let form = ref({} as Todo);
 
 const submit = async () => {
-  addTodo(form.value);
-  await router.push('/');
+  if (!isDateValid(form.value.deadline)) {
+    alert('Invalid date format');
+  } else if (!form.value.title) {
+    alert('Title cannot be empty');
+  } else {
+    addTodo(form.value);
+    await router.push('/');
+  }
 };
 const goBack = () => {
   router.back();
@@ -31,6 +38,7 @@ const goBack = () => {
     <div class="w-full md:px-0 px-8 pb-8 md:w-96 md:mx-auto">
       <input-label label="Deadline"></input-label>
       <input
+        placeholder="YYYY/MM/DD"
         class="w-full px-4 bg-slate-300 text-slate-800 h-10"
         type="text"
         v-model="form.deadline"
@@ -39,14 +47,10 @@ const goBack = () => {
     <div
       class="fixed md:relative md:mt-4 md:w-96 md:mx-auto md:gap-2 flex justify-center bottom-0 w-full"
     >
-      <button
-        class="h-full py-4 md:py-2 w-full bg-green-500"
-        @click="submit"
-        id="save-todo"
-      >
+      <button class="h-full py-2 w-full bg-green-500" @click="submit" id="save-todo">
         Save
       </button>
-      <button class="h-full py-4 md:py-2 w-full bg-red-500" @click="goBack" id="go-back">
+      <button class="h-full py-2 w-full bg-red-500" @click="goBack" id="go-back">
         Go Back
       </button>
     </div>
