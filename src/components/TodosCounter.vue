cc97d9c06c85d8544bfd4c3137748adb01cf17d2
Pietka Tomasz <tomaszpietka92@gmail.com>
2022-07-06 14:05:21 +0200

    Fix todo counter border color for dark mode

--- src/components/TodosCounter.vue
+++ src/components/TodosCounter.vue
@@ -5,7 +5,7 @@ defineProps<{
 </script>
 
 <template>
-  <div class="rounded-full border-2 w-8 h-8">
+  <div class="rounded-full border-2 dark:border-slate-100 border-slate-800 w-8 h-8">
     <div
       class="counter flex justify-center items-center self-center h-full text-lg font-medium"
     >
