9350f8b7529002d086173d63e8d4cc1aa158059c
Pietka Tomasz <tomaszpietka92@gmail.com>
2022-07-07 08:37:16 +0200

    Change todos counter size

--- src/components/TodosCounter.vue
+++ src/components/TodosCounter.vue
@@ -5,9 +5,9 @@ defineProps<{
 </script>
 
 <template>
-  <div class="rounded-full border-2 dark:border-slate-100 border-slate-800 w-8 h-8">
+  <div class="rounded-full border-2 dark:border-slate-100 border-slate-800 w-6 h-6">
     <div
-      class="counter flex justify-center items-center self-center h-full text-lg font-medium"
+      class="counter flex justify-center items-center self-center h-full text-sm font-medium"
     >
       {{ counter }}
     </div>
