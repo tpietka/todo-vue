b551376ab441bb31c017f3543b8d706210eb8f9e
Pietka Tomasz <tomaszpietka92@gmail.com>
2022-07-12 19:37:14 +0200

    Add alert component and store

---
+++ src/components/Alert.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { useAlerts } from '../stores/alerts';
+import { computed, toRefs } from 'vue';
+
+const { message, display, type } = toRefs(useAlerts());
+const color = computed(() => {
+  if (type.value === 'warning') {
+    return 'dark:bg-orange-500 bg-orange-500';
+  } else if (type.value === 'negative') {
+    return 'dark:bg-red-500 bg-red-500';
+  } else {
+    return 'dark:bg-green-500 bg-green-500';
+  }
+});
+</script>
+
+<template>
+  <div class="w-full flex justify-center">
+    <div
+      v-if="display"
+      :class="[
+        color,
+        'fixed h-10 flex justify-center items-center text-slate-300 dark:text-slate-800 top-0 w-full lg:w-1/4',
+      ]"
+    >
+      {{ message }}
+    </div>
+  </div>
+</template>
---
+++ src/stores/alerts.ts
@@ -0,0 +1,29 @@
+import { defineStore } from 'pinia';
+
+interface StoreState {
+  display: boolean;
+  message: string;
+  type: string;
+}
+export const useAlerts = defineStore('alerts', {
+  state: (): StoreState => {
+    return {
+      display: false,
+      message: '',
+      type: '',
+    };
+  },
+  getters: {},
+  actions: {
+    displayAlert(message: string, type: string): void {
+      this.message = message;
+      this.type = type;
+      this.display = true;
+      setInterval(() => {
+        this.display = false;
+        this.message = '';
+        this.type = '';
+      }, 4000);
+    },
+  },
+});
