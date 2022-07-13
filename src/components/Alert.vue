77955dceadd29be2ab422bbc0cb127a66e5f5b47
Pietka Tomasz <tomaszpietka92@gmail.com>
2022-07-13 11:05:33 +0200

    Update alerts colors

--- src/components/Alert.vue
+++ src/components/Alert.vue
@@ -5,7 +5,7 @@ import { computed, toRefs } from 'vue';
 const { message, display, type } = toRefs(useAlerts());
 const color = computed(() => {
   if (type.value === 'warning') {
-    return 'dark:bg-orange-500 bg-orange-500';
+    return 'dark:bg-yellow-400 bg-yellow-400';
   } else if (type.value === 'negative') {
     return 'dark:bg-red-500 bg-red-500';
   } else {
@@ -20,7 +20,7 @@ const color = computed(() => {
       v-if="display"
       :class="[
         color,
-        'fixed h-10 flex justify-center items-center text-slate-300 dark:text-slate-800 top-0 w-full lg:w-1/4',
+        'fixed h-10 flex justify-center items-center font-bold text-slate-800 dark:text-slate-800 top-0 w-full lg:w-1/4',
       ]"
     >
       {{ message }}
