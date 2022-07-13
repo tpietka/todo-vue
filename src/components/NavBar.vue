5c8fa8e0de763e084d823edbef3e8d3623793e58
Pietka Tomasz <tomaszpietka92@gmail.com>
2022-07-13 13:20:38 +0200

    Move top elements to seprate NavBar component

---
+++ src/components/NavBar.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { toRefs } from 'vue';
+import { getCurrentDayName, getCurrentDateDDMMYYYY } from '../helpers/date';
+import { useSettings } from '../stores/settings';
+const { setDark, setLight } = useSettings();
+const { theme } = toRefs(useSettings());
+</script>
+
+<template>
+  <nav class="w-full text-slate-800 dark:text-slate-300 dark:bg-slate-800 bg-slate-300">
+    <div class="flex lg:w-10/12 mx-6 lg:mx-auto items-center">
+      <div @click="$router.push('/')" class="text-2xl w-full">MyTodos</div>
+      <div class="w-full flex justify-center">
+        <span
+          v-if="theme != 'dark'"
+          class="material-icons cursor-pointer"
+          @click="setDark"
+          >dark_mode</span
+        >
+        <span
+          v-if="theme == 'dark'"
+          class="material-icons cursor-pointer"
+          @click="setLight"
+          >light_mode</span
+        >
+      </div>
+      <div class="text-right w-full flex-col">
+        <div class="text-lg">{{ getCurrentDayName() }}</div>
+        <div class="text-sm">{{ getCurrentDateDDMMYYYY() }}</div>
+      </div>
+    </div>
+  </nav>
+</template>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,35 +1,11 @@
 <script setup lang="ts">
-import { toRefs } from 'vue';
-import { getCurrentDayName, getCurrentDateDDMMYYYY } from '../helpers/date';
-import { useSettings } from '../stores/settings';
-const { setDark, setLight } = useSettings();
-const { theme } = toRefs(useSettings());
+import NavBar from '../components/NavBar.vue';
 </script>
 
 <template>
   <div class="h-full w-full text-slate-800 dark:text-slate-100">
+    <nav-bar></nav-bar>
     <div class="h-full lg:w-10/12 lg:mx-auto py-4 mb-10 lg:py-8">
-      <div class="flex justify-between mx-6 lg:mx-0">
-        <div class="text-4xl w-full">MyTodos</div>
-        <div class="mt-2 w-full flex justify-center">
-          <span
-            v-if="theme != 'dark'"
-            class="material-icons cursor-pointer"
-            @click="setDark"
-            >dark_mode</span
-          >
-          <span
-            v-if="theme == 'dark'"
-            class="material-icons cursor-pointer"
-            @click="setLight"
-            >light_mode</span
-          >
-        </div>
-        <div class="text-right w-full flex-col">
-          <div class="text-2xl">{{ getCurrentDayName() }}</div>
-          <div>{{ getCurrentDateDDMMYYYY() }}</div>
-        </div>
-      </div>
       <router-view></router-view>
     </div>
   </div>
