a965bc53185d9ddf6e4390323fe61c21c628784b
Pietka Tomasz <tomaszpietka92@gmail.com>
2022-07-13 11:06:16 +0200

    Add validation error message component

---
+++ src/components/ValidationErrorMessage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="text-red-500" v-if="displayError">
+    {{ messageToDisplay }}
+  </div>
+</template>
+<script setup lang="ts">
+import { ErrorObject } from '@vuelidate/core';
+import { computed } from 'vue';
+
+const props = defineProps<{
+  displayError: boolean;
+  errorMessages: ErrorObject[];
+}>();
+
+const messageToDisplay = computed((): string => {
+  return props.errorMessages[0].$message as string;
+});
+</script>
