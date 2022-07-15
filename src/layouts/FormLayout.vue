4019050fea2584f0147be79543ad89b15ca5f1a2
Pietka Tomasz <tomaszpietka92@gmail.com>
2022-07-15 16:58:08 +0200

    Add filter component to main layout

--- src/layouts/FormLayout.vue
+++ src/layouts/FormLayout.vue
@@ -1,13 +1,11 @@
 <script setup lang="ts">
 import NavBar from '../components/NavBar.vue';
-import Filters from '../components/Filters.vue';
 </script>
 
 <template>
   <div class="h-full w-full text-slate-800 dark:text-slate-100">
     <nav-bar></nav-bar>
     <div class="h-full lg:w-10/12 lg:mx-auto my-24">
-      <filters></filters>
       <router-view></router-view>
     </div>
   </div>
--- src/router.ts
+++ src/router.ts
@@ -6,10 +6,15 @@ const routes = [
     children: [
       { path: '/', name: 'Home', component: () => import('./views/Home.vue') },
       { path: '/archive', name: 'Archive', component: () => import('./views/Archive.vue') },
+    ]
+  },
+  {
+    path: '/', name: 'FormLayout', component: () => import('./layouts/FormLayout.vue'),
+    children: [
       { path: '/add-todo/', name: 'NewTodoForm', component: () => import('./views/Form.vue') },
       { path: '/edit-todo/:id', name: 'EditTodoForm', component: () => import('./views/Form.vue'), props: true }
     ]
-  },
+  }
 ] as RouteRecordRaw[];
 
 export const router = createRouter({
